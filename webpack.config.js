b5704c0473a8761a35cec17a6f669943e8e25207
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-07-23 13:16:36 +0200

    Install optimize js plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
 
-plugins = [new HtmlWebpackPlugin({
+const plugins = [new HtmlWebpackPlugin({
 	template: 'src/index.html',
 	filename: 'index.html',
 	inject: 'body'
-})]
+})];
 
 module.exports = (env) => {
 	const environment = env || 'production';
+	if (env === 'production') {
+		plugins.push(
+			new OptimizeJsPlugin({
+				sourceMap: false
+ 			})
+		)
+	}
 
 	return {
 		mode: environment,
@@ -36,6 +44,7 @@ module.exports = (env) => {
 					]
 				}
 			]
-		}
+		},
+		plugins
 	}
 };
